505febdfcff1cf86b44d67d08d1111d4760d1173
aljbartee-ui <al.jbartee@gmail.com>
2025-11-02 09:47:28 +0300

    Update user.py
    to apply V2.0 again

--- src/models/user.py
+++ src/models/user.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -9,6 +10,20 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+class AdminUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return f'<AdminUser {self.username}>'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def to_dict(self):
         return {
